f1ca53813b5f6898c9f4cc53f6a58f4c0c3712b7
crystalcrystal-lim <69325538+crystalcrystal-lim@users.noreply.github.com>
2020-12-04 10:32:19 +1300

    header and home component created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-import { fetchFruits } from '../actions'
+import Header from './Header'
+import Home from './Home'
+
+
 
 export class App extends React.Component {
-  state = {
-    fruits: []
-  }
-
-  componentDidMount() {
-    this.props.dispatch(fetchFruits())
-  }
 
   render() {
     return (
       <div className='app'>
-        <h1>Fullstack Boilerplate - with Liszts!</h1>
-        <ul>
-          {this.props.fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
+        <Header/>
+      <Home/>
       </div>
     )
   }
 }
 
-function mapStateToProps(globalState) {
-  return {
-    fruits: globalState.fruits
-  }
-}
 
-export default connect(mapStateToProps)(App)
+export default App
---
+++ client/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Signin from './Signin'
+
+export class Home extends React.Component {
+
+
+  render() {
+    return (
+      <div className='Home'>
+        <Signin/>
+      </div>
+    )
+  }
+}
+
+
+export default Home
