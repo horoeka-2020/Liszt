63063980b7a4d0f95a2032209739fe2c34f8b7fd
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-12-05 11:46:31 +1300

    created post route

--- server/books.js
+++ server/books.js
@@ -12,15 +12,28 @@ router.get('/books/:userId', (req, res) => {
     .catch(err => {
       res.status(500).json({ error: err.message })
     })
+})                                                                                                                                                                                                                                                                                                           
+
+router.post('/books/:userId', (req, res) => {
+  const { user_id, book_api_id, image_url } = req.body
+  const newBook =  { user_id, book_api_id, image_url }
+  db.addBooksToBookList(newBook)
+  .then((books) => {
+    res.json(books)
+    return null
+  })
+  .catch(err => {
+    res.status(500).json({ error: err.message })
+  })
 })
 
-router.get('/books/:userId/:id', (req, res) => {
-  db.getUserBookById(req.params.userId, req.params.id)
-    .then(book => {
-      return res.json(book)
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message })
-    })
-})
+// router.get('/books/:userId/:id', (req, res) => {
+//   db.getUserBookById(req.params.userId, req.params.id)
+//     .then(book => {
+//       return res.json(book)
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: err.message })
+//     })
+// })
 module.exports = router
