906dbded1969c40f2714131b486737241e252f23
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-12-04 14:39:32 +1300

    redone our migrations and seeds

--- server/db/migrations/20201204103945_book_list.js
+++ server/db/migrations/20201204103945_book_list.js
@@ -2,10 +2,7 @@ exports.up = (knex) => {
   return knex.schema.createTable('book_list', table => {
     table.increments('id').primary()
     table.integer('user_id').references('users.id')// how to make a foreign key
-    table.string('title')
-    table.string('author')
-    table.string('published_date')
-    table.varchar('description')
+    table.string('book_api_id')
     table.string('image_url')
   })
 }
