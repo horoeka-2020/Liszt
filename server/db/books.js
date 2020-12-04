7aef06d836ce2aa94875eac33e91025ccd4ea67c
KennethCruz <30338645+Piecre24@users.noreply.github.com>
2020-12-04 17:56:02 +1300

    lintin

--- server/db/books.js
+++ server/db/books.js
@@ -2,25 +2,25 @@ const { connect } = require('superagent')
 const connection = require('./connection')
 
 module.exports = {
-    getUserBooksList,
-    getUserBookById,
-    addBooksToBookList
+  getUserBooksList,
+  getUserBookById,
+  addBooksToBookList
 }
 // gets the books of the user
-function getUserBooksList (userId, db = connection){
-    return db('book_list').where('user_id', userId).select('id', 'user_id as userId', 'book_api_id as bookApiId', 'image_url as imageUrl')
+function getUserBooksList (userId, db = connection) {
+  return db('book_list').where('user_id', userId).select('id', 'user_id as userId', 'book_api_id as bookApiId', 'image_url as imageUrl')
 }
 // gets one book of the user
 function getUserBookById (userId, id, db = connection) {
-    return db('book_list').where('id', id, 'user_id', userId).select().first()
+  return db('book_list').where('id', id, 'user_id', userId).select().first()
 }
 
 // soon
 function addBooksToBookList (newBook, db = connect) {
-    return db('book_list')
+  return db('book_list')
     .insert({
-        user_id: newBook.user_id,
-        book_api_id: newBook.book_api_id,
-        image_url: newBook.image_url
+      user_id: newBook.user_id,
+      book_api_id: newBook.book_api_id,
+      image_url: newBook.image_url
     })
 }
