5ace6ba631da0dff0e22354c1222323ff7e92d24
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-12-05 15:57:01 +1300

    Add routes for postBookToList, delBookFromList

--- client/apis/books.js
+++ client/apis/books.js
@@ -1,13 +1,49 @@
+// Remaining question:
+// For the add and delete book features, if those actions are successful, we want to take the user back to their newly updated list.
+// Question is; who asks for that booklist? Booklist.jsx who renders it? Or should the list be returned back here after the delete?
+
 import request from 'superagent'
 
-// Won't use rootURL. Delete if can get these routes to work
-// const rootURL = '/api/v1/books'
-
 export function getBookList (user) {
+  // To be called by BookList.jsx so it can map books to render the Carousel.
+  // Complete, pending tests
+
   const hardCodedUserId = 1
   return request.get(`/api/v1/books/${hardCodedUserId}`)
     .then((res) => {
       console.log(res)
-      return res.body.books
+      return res.body
+    })
+}
+
+export function postBookToList (userId, bookApiId) {
+  // To be called when user clicks Add button on BookDetail.jsx
+  // Sending book_api_id and user_id to serverside to add to user's booklist
+
+  const bookDetails = {
+    userId: userId,
+    bookApiId: bookApiId
+  }
+  return request.post('/api/v1/books/')
+    .send(bookDetails)
+    .then((res) => {
+      console.log(res)
+      return res.body
+    })
+}
+
+export function delBookFromList (userId, bookApiId) {
+  // To be called when user clicks Remove button on BookDetail.jsx
+  // Sending book_api_id and user_id to serverside to remove from user's booklist
+
+  const bookDetails = {
+    userId: userId,
+    bookApiId: bookApiId
+  }
+  return request.del('/api/v1/books/')
+    .send(bookDetails)
+    .then((res) => {
+      console.log(res)
+      return res.body
     })
 }
