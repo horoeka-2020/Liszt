616e1aa0bc9aad684b53a2054342cd75e3a027b1
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-12-06 16:36:33 +1300

    wrestle with componentdidmount in carousel

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import M from 'materialize-css'
-import 'materialize-css/dist/css/materialize.min.css'
+// import M from 'materialize-css'
+// import 'materialize-css/dist/css/materialize.min.css'
 import CarouselImage from './CarouselImage'
 import { getBookList } from '../apis/books'
 
@@ -11,33 +11,38 @@ export default class Carousel extends React.Component {
 
   componentDidMount () {
     getBookList(8)
-      .then(books =>
+      .then((books) => {
         this.setState({
           books: books
-        }))
+        })
+        console.log(this.state.books)
+      })
   }
 
-  fetchPhotos () {
-    const options = {
-      duration: 300,
-      onCycleTo: () => {
-        console.log('New Slide')
-      }
-    }
-    M.Carousel.init(this.Carousel, options)
-  }
+  // fetchPhotos () {
+  //   const options = {
+  //     duration: 300,
+  //     onCycleTo: () => {
+  //       console.log('New Slide')
+  //     }
+  //   }
+  //   M.Carousel.init(this.Carousel, options)
+  // }
 
   render () {
-    console.log('i am here', this.state.books)
+    console.log('this', this.state.books)
     return (
       <div
-        ref={(Carousel) => {
-          this.Carousel = Carousel
-        }}
+        // ref={(Carousel) => {
+        //   this.Carousel = Carousel
+        // }}
         className='carousel'
       >
-        {this.state.books.map((book) => {
-        })}
+
+        {/* <p>{this.state.books}</p> */}
+        {/* {this.state.books.map((book) => {
+          <h1 key={book.id}>{book.imageUrl}</h1>
+        })} */}
       </div>
     )
   }
