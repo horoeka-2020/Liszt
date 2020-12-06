919542d8d4efa7bc9ce04d13b9aad2572de20582
al-abbas-nz <al.mus930@gmail.com>
2020-12-06 19:30:00 +1300

    log this.state.books.books

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,49 +1,62 @@
 import React from 'react'
 // import M from 'materialize-css'
-// import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css'
 import CarouselImage from './CarouselImage'
 import { getBookList } from '../apis/books'
 
-export default class Carousel extends React.Component {
+class Carousel extends React.Component {
   state = {
     books: []
   }
-
-  componentDidMount () {
-    getBookList(8)
-      .then((books) => {
-        this.setState({
-          books: books
-        })
-        console.log(this.state.books)
+  componentDidMount() {
+    getBookList(8).then((books) =>
+      this.setState({
+        books: books
       })
-  }
-
-  // fetchPhotos () {
-  //   const options = {
-  //     duration: 300,
-  //     onCycleTo: () => {
-  //       console.log('New Slide')
-  //     }
-  //   }
-  //   M.Carousel.init(this.Carousel, options)
-  // }
-
-  render () {
-    console.log('this', this.state.books)
-    return (
-      <div
-        // ref={(Carousel) => {
-        //   this.Carousel = Carousel
-        // }}
-        className='carousel'
-      >
-
-        {/* <p>{this.state.books}</p> */}
-        {/* {this.state.books.map((book) => {
-          <h1 key={book.id}>{book.imageUrl}</h1>
-        })} */}
-      </div>
     )
   }
+  render() {
+    return <div>{console.log(this.state.books.books)}</div>
+  }
 }
+
+export default Carousel
+
+// export default class Carousel extends React.Component {
+//   state = {
+//     books: []
+//   }
+
+//   componentDidMount() {
+//     getBookList(8).then((books) => {
+//       this.setState({
+//         books: books
+//       })
+//       console.log(this.state.books)
+//     })
+//   }
+
+//   // fetchPhotos () {
+//   //   const options = {
+//   //     duration: 300,
+//   //     onCycleTo: () => {
+//   //       console.log('New Slide')
+//   //     }
+//   //   }
+//   //   M.Carousel.init(this.Carousel, options)
+//   // }
+
+//   render() {
+//     console.log('this', this.state.books)
+//     return (
+//       <div
+//       // ref={(Carousel) => {
+//       //   this.Carousel = Carousel
+//       // }}
+//       // className='carousel'
+//       >
+//         <h1>carousel!</h1>
+//       </div>
+//     )
+//   }
+// }
