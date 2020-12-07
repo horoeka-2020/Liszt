a7cab0602d8bd38f2aadbe7d79ff932b406538cc
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-12-07 18:41:49 +1300

    Add FilmLibrary find book, replace film

---
+++ client/components/films/FilmLibrary.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import 'materialize-css/dist/css/materialize.min.css'
+import FilmCard from './FilmCard'
+import { getFilmList } from '../../apis/films'
+
+class FilmLibrary extends React.Component {
+  state = {
+    films: []
+  }
+
+  componentDidMount () {
+    getFilmList(8).then((films) =>
+      this.setState({
+        films: films.films
+      })
+    )
+  }
+
+  refreshList = (films) => {
+    this.setState({
+      films: films
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.films.map((film) => {
+          return <FilmCard author={film.author}
+            title={film.title}
+            image={film.imageUrl}
+            isbn={film.filmApiId}
+            refreshList={this.refreshList}
+            key={film.filmApiId}/>
+        })}
+      </div>
+    )
+  }
+}
+
+export default FilmLibrary
