cefdeb215a49fd2a8ea503c5a2e638a286283977
Al Abbas <65952319+al-abbas-nz@users.noreply.github.com>
2020-12-08 14:41:00 +1300

    routes created for music api

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -1,1 +1,43 @@
 const express = require('express')
+
+const db = require('../db/songs')
+
+const router = express.Router()
+
+router.get('/:userId', (req, res) => {
+  db.getUserSongsList(req.params.userId)
+    .then(songs => {
+      return res.json({ songs })
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
+router.post('/', (req, res) => {
+  const { userId } = req.body
+  const newSong = { userId }
+  db.addSongsToSongList(newSong)
+    .then(() => db.getUserSongsList(userId))
+    .then((songs) => {
+      res.json(songs)
+      return null
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
+router.delete('/', (req, res) => {
+  const { songApiId, userId } = (req.body)
+  db.removeSongsFromSongList(songApiId, userId)
+    .then(() => db.getUserSongsList(userId))
+    .then((songs) => {
+      res.json(songs)
+      return null
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+module.exports = router
